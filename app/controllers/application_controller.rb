dc59c72b7f2d25d5f29436d0978bccb7a2fed222
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2019-10-20 10:13:57 +0000

    restrict actions, views and clean up layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  before_action :require_user
+  
   helper_method :current_user, :logged_in?
   
   def current_user
@@ -11,4 +13,10 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
   
+  def require_user
+    if !logged_in?
+      flash[:notice] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
