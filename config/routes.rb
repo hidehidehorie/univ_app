d73f7e462338ab0eba793ae94114bab612e05246
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2019-10-20 04:26:28 +0000

    Add login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   
   resources :students, except: [:destroy]
+  
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
